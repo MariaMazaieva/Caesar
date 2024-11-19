93eb6fc4180a3dc5d7e41d1e7a2e7fd3196e87e7
MariaMazaieva <mazaevamasha@gmail.com>
2024-11-19 18:40:51 +0100

    100% Working required task

--- main.c
+++ main.c
@@ -7,44 +7,30 @@
 #define Difference 39
 #define Z 90
 #define INIT_SIZE 20
-// char* control_input( char* arr_first, int*length_1);
-int control_input( char** arr_first, size_t * size, size_t *capacity);
+int control_input( char** arr_first, unsigned long* size, unsigned long *capacity, unsigned long* length);
 
-void shift(char* arr_first, const char * arr_second, int offset, char* letters[]);
+void shift(char* arr_first, const char * arr_second);
 int compare(const char *str1, const char *str2);
-char rotate(char original[], int offset,  char* letters[]);
+void rotate(char original[], int offset,  const char* letters);
 
-// int* error_func(int *ret, char input);
 
 int main(void) {
-    size_t size = 0;
-    size_t capacity = INIT_SIZE;
-    int length_1 =0,length_2 = 0;
+    unsigned long size = 0;
+    unsigned long capacity = INIT_SIZE;
+    unsigned long length_1 =0;
+    unsigned long length_2 = 0;
     int ret = EXIT_SUCCESS;
-    int offset = 0;
-    char *arr_first = (char*)malloc(sizeof(char) * capacity);
-    char *arr_second  = (char*)malloc(sizeof(char) * capacity);
-    // char* arr_first [15];
+    char *arr_first = (char*)calloc(capacity, sizeof(char));
+    char *arr_second = (char*)calloc(capacity, sizeof(char));
     if (arr_first != NULL && arr_second != NULL) { //if not empty
-        control_input(&arr_first, &size, &capacity);
-        printf("ret = %d\n", ret);
-        // printf("res is %d\n", res);
-        // control_input(&arr_first, size, capacity);
-        // printf("%c", k);
-        // printf("ABC %c\n", arr_first[0]);
-        // control_input(&arr_second, &length_2);
+        control_input(&arr_first, &size, &capacity, &length_1);
+        size = 0;
+        capacity = INIT_SIZE;
+        control_input(&arr_second, &size, &capacity, &length_2);
     }else {
         free ( arr_first );
-        // free ( arr_second );
+        free ( arr_second );
     }
-    char * letters[] ={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    };
-    // char letter = (arr_first[3] - 'a' + offset)%26 +'a';
-    // printf("First %c\n", letter);
-    // printf("%c\n",letters[51]);
 
     if(arr_first == NULL) {
         ret = EXIT_FAILURE;
@@ -52,55 +38,70 @@ int main(void) {
     }
     //Check for length
 
-    // if(length_1 != length_2) {
-    //     fprintf(stderr,"Chybna delka vstupu!\n");
-    //     free(arr_first);
-    //     free(arr_second);
-    //     ret = 101;
-    // }
+    // printf("rrr2 %c\n", arr_second[2]);
+    if(length_1 != length_2 ) {
+        fprintf(stderr,"Error: Chybna delka vstupu!\n");
+        free(arr_first);
+        free(arr_second);
+        return  101;
+    }
 
-    // if(ret == EXIT_SUCCESS)
-        // shift(arr_first,  arr_second, offset, letters);
+    if(ret == EXIT_SUCCESS)
+        shift(arr_first,  arr_second);
 
     free(arr_first);
-    // free(arr_second);
+    free(arr_second);
 
     return ret;
 }
 
 
-void shift(char* arr_first, const char * arr_second, int offset, char* letters[]) { // WHY TF IT HAS CHANGED AFTER I ADDED []
+void shift(char* arr_first, const char * arr_second) { // WHY TF IT HAS CHANGED AFTER I ADDED []
     int counter = 0;
-    offset = 0;
+    int offset = 0;
     int max_same = 0;
-    char tamp;
-    // while(offset != 52){
-    // char re = letters[('a' - 'a' + 2) ];
-    // printf("A%c", letters[28]);
-    // printf("re is %c\n", re);
-    size_t size = strlen(arr_first);
+    char tamp = 0;
+    int new_letter = 0;
+    char letters[] = {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    };
 
-    for (int j = 0; j < Alphabet; j++) {
-        for (int i = 0; i < size-1; i++) {//////dont forget
+    unsigned long size = strlen(arr_first);
+    // printf("leters=%s\n", letters[3]);
+    // printf("leters=%c\n", letters[2]);
+    for (int j = 0; j < 26; j++) {
+        // if(j == arr_first[0] - 'a') {
+        //     printf("Break at %c\n",j);
+        //     break;
+        // }
+        for (int i = 0; i < size; i++) {//////dont forget
             // int posun = (j + (arr_first[i] - 'a'));//%52
             // tamp = letters[posun];
             // printf("arr i %c is i %d, j is %d ", arr_first[i], i, j);
             // printf("tamp=%c ", tamp);
 
             if(arr_first[i] < After_Z && j + arr_first[i] > Z) {
-                tamp = *letters[j + arr_first[i] - After_Z];
+                tamp = letters[j + arr_first[i] - After_Z];
             }
             else if (arr_first[i] < After_Z) {
-                tamp = *letters[j + arr_first[i] - Difference];
+                tamp = letters[j + arr_first[i] - Difference];
             }
             else {
-                int new_letter = j + (arr_first[i] - 'a');
-                tamp = *letters[new_letter];
+                new_letter = j + (arr_first[i] - 'a');
+                tamp = letters[new_letter];
             }
             if(tamp == arr_second[i]) {
                 counter ++;
             }
-            // printf("arr i %c is i %d, j is %d ", arr_first[i], i, j);
+            // printf("tamp=%c\n", tamp);
+            // printf("new_letter=%d\n", new_letter);
+            // printf("arr_second=%c\n", arr_second[i]);
+            // printf("leters=%c\n", letters[new_letter]);
+
+            // printf("arr_1 %c is in i %d, j is %d ", arr_first[i], i, j);
             // printf("tamp=%c ", tamp);
             // printf("counter=%d\n", counter);
 
@@ -112,25 +113,28 @@ void shift(char* arr_first, const char * arr_second, int offset, char* letters[]
         counter = 0;
         // printf("\n");
     }
+
     rotate(arr_first, offset, letters);///////////////////////////////////////
     // printf("max_same %d\n", max_same);
     // printf("offset is %d\n", offset);
 }
-
-char rotate(char original[], int offset, char* letters[]) {
+//qrstuvwxyz
+//aHcQefghWj
+void rotate(char original[], const int offset, const char* letters) {
     char tamp;
-    size_t size = strlen(original);
-    for(int i = 0; i < size-1; i++) {
+    unsigned long size = strlen(original);//VALGRIN
+    for(int i = 0; i < size; i++) {
         if(original[i] < After_Z && offset + original[i] > Z) {
-            tamp = *letters[offset + original[i] - After_Z];
+            tamp = letters[offset + original[i] - After_Z];
         }
         else if (original[i] < After_Z) {
-            tamp = *letters[offset + original[i] - Difference];
+            tamp = letters[offset + original[i] - Difference];
         }
         else {
             int new_letter = offset + (original[i] - 'a');
-            tamp = *letters[new_letter];
+            tamp = letters[new_letter];
         }
+        // printf("original%c\n", original[i]);
 
         printf("%c", tamp);
     }
@@ -181,55 +185,50 @@ char rotate(char original[], int offset, char* letters[]) {
 //     *length_1 = counter-1;
 //     return arr_first;
 // }
-int control_input(char** arr_first, size_t * size, size_t *capacity) {
+int control_input(char** arr_first, unsigned long* size, unsigned long *capacity, unsigned long* length) {
     //also works for arr2 if i dont change it here ???
     int ret = EXIT_SUCCESS;
-    char input;
     while(1) {
-        printf("First %lu\n", *size);
-        input = getchar();
+        // printf("First %lu\n", *size);
+        char input = getchar();
         if(input == '\n') {
             break;
         }
         if ((input < 'A' || (input > 'Z' && input < 'a') || input > 'z')){ //CHECK for \n, free
             fprintf(stderr, "Error: Chybny vstup!\n");
-            ret =100;
-            break;
+            exit(100);
+            // ret =100;
+            // break;
         }
             if (*size >= *capacity) {
                 char *temp = (char*)realloc(*arr_first, sizeof(char) * (*capacity) * 2);
                 if (temp == NULL) {
                     free(*arr_first);
+                    *arr_first = NULL;
+                    // return EXIT_FAILURE;
                     break;
                 }else {
-                    *capacity = *capacity * 2;
                     *arr_first = temp;
+                    *capacity = *capacity * 2;
                 }
 
             }
-        printf("Second %lu\n", *size);
-        printf("size3 %lu, capacity3 %lu\n", *size, *capacity);
+        // printf("Second %lu\n", *size);
+        // printf("size3 %lu, capacity3 %lu\n", *size, *capacity);
         (*arr_first)[*size] = input;/// -  (*arr_first) AAAAAAAAAAAAAAA
+        // printf("prvek is %c \n",  (*arr_first)[*size]);
         (*size)++;
     }
-    
-    printf("size3 %lu, capacity3 %lu\n", *size, *capacity);
-    char k = *arr_first[4];
-    char k2 = *arr_first[21];
-    printf("Leter %c \n", *(arr_first)[21]);
-    printf("k %c, k2 %c \n", k,k2);
-    // int velikost = sizeof(*arr_first)/sizeof(char);
-    // printf("Velikost %d \n", velikost);
-    // printf("Array size  %lu\n",sizeof(*arr_first));
+    char *tamp = (char*)realloc(*arr_first, sizeof(char) * (*size+1)); //for \n
+    if (tamp == NULL) {
+        free(*arr_first);
+        *arr_first = NULL;
+        ret = 108;
+        return ret;
+    }
+    *arr_first = tamp;
+    *length = *size;
     return ret;
 }
-// int* error_func(int *ret, char input) {
-//     if (input != '\n' && (input < 'A' || (input > 'Z' && input < 'a') || input > 'z')) { //CHECK for \n, free
-//         // free(arr_first);
-//         fprintf(stderr, "Error: Chybny vstup!\n");
-//         *ret = 100;
-//     }
-//     return ret;
-// }
 
 
